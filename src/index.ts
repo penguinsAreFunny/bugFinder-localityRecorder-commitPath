48f4a54fe70cff6c33db9728bdf6fdec8300d779
Robert Klinger <robert@k1inger.de>
2021-09-21 15:19:22 +0200

    fix: removed default binding for CommitPathRecorder as it is irritating to inject TYPES of CommitPathRecorder without injecting CommitPathRecorder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import {localityAContainer} from "bugfinder-framework";
 import {BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES} from "./TYPES";
 import {CommitToCommitPathMapper, DefaultCommitPathMapper} from "./commitToCommitPath";
-import {CommitRecorder} from "bugfinder-localityrecorder-commit";
 
 export * from "./commitPath"
 export * from "./commitPathRecorder"
@@ -9,4 +8,3 @@ export * from "./commitToCommitPath"
 export * from "./TYPES"
 
 localityAContainer.bind<CommitToCommitPathMapper>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitToCommitPathMapper).to(DefaultCommitPathMapper)
-localityAContainer.bind<CommitRecorder>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitRecorder).to(CommitRecorder)
