150cc8f364a2a9aa8af25a1b6ebacfb976c0810a
Robert Klinger <robert@k1inger.de>
2021-09-21 14:40:42 +0200

    fix: wrong container used for default config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {localityBContainer} from "bugfinder-framework";
+import {localityAContainer} from "bugfinder-framework";
 import {BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES} from "./TYPES";
 import {CommitToCommitPathMapper, DefaultCommitPathMapper} from "./commitToCommitPath";
 import {CommitRecorder} from "bugfinder-localityrecorder-commit";
@@ -8,5 +8,5 @@ export * from "./commitPathRecorder"
 export * from "./commitToCommitPath"
 export * from "./TYPES"
 
-localityBContainer.bind<CommitToCommitPathMapper>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitToCommitPathMapper).to(DefaultCommitPathMapper)
-localityBContainer.bind<CommitRecorder>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitRecorder).to(CommitRecorder)
+localityAContainer.bind<CommitToCommitPathMapper>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitToCommitPathMapper).to(DefaultCommitPathMapper)
+localityAContainer.bind<CommitRecorder>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitRecorder).to(CommitRecorder)
