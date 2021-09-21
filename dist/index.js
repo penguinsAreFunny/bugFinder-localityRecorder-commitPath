f561969c3721b3645e484ce5cb4dd70c552bf833
Robert Klinger <robert@k1inger.de>
2021-09-21 14:30:05 +0200

    1.1.0

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,14 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var bugfinder_framework_1 = require("bugfinder-framework");
+var TYPES_1 = require("./TYPES");
+var commitToCommitPath_1 = require("./commitToCommitPath");
+var bugfinder_localityrecorder_commit_1 = require("bugfinder-localityrecorder-commit");
 __exportStar(require("./commitPath"), exports);
 __exportStar(require("./commitPathRecorder"), exports);
 __exportStar(require("./commitToCommitPath"), exports);
 __exportStar(require("./TYPES"), exports);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEsK0NBQTRCO0FBQzVCLHVEQUFvQztBQUNwQyx1REFBb0M7QUFDcEMsMENBQXVCIn0=
+bugfinder_framework_1.localityAContainer.bind(TYPES_1.BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitToCommitPathMapper).to(commitToCommitPath_1.DefaultCommitPathMapper);
+bugfinder_framework_1.localityAContainer.bind(TYPES_1.BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitRecorder).to(bugfinder_localityrecorder_commit_1.CommitRecorder);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEsMkRBQXVEO0FBQ3ZELGlDQUFvRTtBQUNwRSwyREFBdUY7QUFDdkYsdUZBQWlFO0FBRWpFLCtDQUE0QjtBQUM1Qix1REFBb0M7QUFDcEMsdURBQW9DO0FBQ3BDLDBDQUF1QjtBQUV2Qix3Q0FBa0IsQ0FBQyxJQUFJLENBQTJCLG1EQUEyQyxDQUFDLHdCQUF3QixDQUFDLENBQUMsRUFBRSxDQUFDLDRDQUF1QixDQUFDLENBQUE7QUFDbkosd0NBQWtCLENBQUMsSUFBSSxDQUFpQixtREFBMkMsQ0FBQyxjQUFjLENBQUMsQ0FBQyxFQUFFLENBQUMsa0RBQWMsQ0FBQyxDQUFBIn0=
