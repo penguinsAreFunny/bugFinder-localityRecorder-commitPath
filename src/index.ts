bece9089fb4b2b782025f4a9fac2a2ef8c880cf7
Robert Klinger <robert@k1inger.de>
2021-09-21 14:29:41 +0200

    Default configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import {localityAContainer} from "bugfinder-framework";
+import {BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES} from "./TYPES";
+import {CommitToCommitPathMapper, DefaultCommitPathMapper} from "./commitToCommitPath";
+import {CommitRecorder} from "bugfinder-localityrecorder-commit";
+
 export * from "./commitPath"
 export * from "./commitPathRecorder"
 export * from "./commitToCommitPath"
 export * from "./TYPES"
+
+localityAContainer.bind<CommitToCommitPathMapper>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitToCommitPathMapper).to(DefaultCommitPathMapper)
+localityAContainer.bind<CommitRecorder>(BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES.commitRecorder).to(CommitRecorder)
