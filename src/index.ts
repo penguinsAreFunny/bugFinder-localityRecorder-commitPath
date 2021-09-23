daf42e3375e6743a344e88bd9de3b620100512f5
Robert Klinger <robert@k1inger.de>
2021-09-23 13:13:20 +0200

    fix: import the right localityAContainer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import {localityAContainer} from "bugfinder-framework";
 import {BUGFINDER_LOCALITYRECORDER_COMMITPATH_TYPES} from "./TYPES";
 import {CommitToCommitPathMapper, DefaultCommitPathMapper} from "./commitToCommitPath";
+import {localityAContainer} from "bugFinder-framework-defaultContainer";
+import * as commitRecorder from "bugfinder-localityrecorder-commit";
 
 export * from "./commitPath"
 export * from "./commitPathRecorder"
