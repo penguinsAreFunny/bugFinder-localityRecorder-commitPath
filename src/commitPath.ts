1fab3cca1851b3096ab582732b6a9e1887e87cde
Robert Klinger <robert@k1inger.de>
2021-09-28 13:50:04 +0200

    refactor: removing PathHandling from localityRecorder

--- src/commitPath.ts
+++ src/commitPath.ts
@@ -2,33 +2,6 @@ import * as crypto from "crypto";
 import {Locality} from "bugfinder-framework";
 import {Commit, GitFile} from "bugfinder-localityrecorder-commit";
 
-/**
- * PathsHandling is used to determine which paths
- * 1. should be used
- * 2. should be injected
- */
-export interface PathsHandling {
-    /**
-     * Filters all CommitPath-Paths which should be used.
-     * The matched files will be quantified
-     * F.e.: \/*.ts\b for just adding paths ending with .ts.
-     */
-    pathIncludes?: RegExp;
-
-    /**
-     * Paths which should be injected and therefore quantified.
-     * F.e. ["src"] if relative values to whole src should be evaluated
-     */
-    injections: string[];
-
-    /**
-     * Determines whether paths should be injected
-     * if the only paths remaining would be the injected ones
-     */
-    injectOnEmptyPaths: boolean;
-}
-
-
 export class CommitPath implements Locality {
 
     /**
